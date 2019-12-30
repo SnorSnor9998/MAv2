a52eb4e6bf08e9ce01365f5cf5c27fa5016f842b
snorsnor9998 <o0ofuckhackero0o>
2019-12-31 01:27:07 +0800

    v1.4.2

--- app/src/main/java/com/example/mav2/addactivity.kt
+++ app/src/main/java/com/example/mav2/addactivity.kt
@@ -1,18 +1,28 @@
 package com.example.mav2
 
 
+import android.app.Activity
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.fragment_addactivity.*
 import java.text.SimpleDateFormat
 import java.util.*
+import android.content.ContentResolver as ContentResolver1
 
 /**
  * A simple [Fragment] subclass.
@@ -22,8 +32,20 @@ class addactivity : Fragment() {
     var formate = SimpleDateFormat("dd/MM/yyyy", Locale.US)
     var timeFormat = SimpleDateFormat("hh:mm a", Locale.US)
 
+    var selectedPhotoUri : Uri? = null
+
     override fun onStart() {
         super.onStart()
+
+
+        cna_butt_uploadphoto.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+
+        }
+
+
 
         cna_time_from.setOnClickListener{
             val now = Calendar.getInstance()
@@ -72,8 +94,103 @@ class addactivity : Fragment() {
         }
 
 
+        cna_button.setOnClickListener {
+
+
+
+            var validate : Boolean = true
+
+            if(cna_date.text.toString().equals("Date Picker")||cna_date.text.toString().equals("Date is required")){
+                cna_date.setText("Date is required")
+                textView22.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(cna_time_from.text.toString().equals("Start")||cna_time_from.text.toString().equals("Required")){
+                cna_time_from.setText(R.string.validate_required)
+                textView15.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(cna_time_to.text.toString().equals("End")||cna_time_to.text.toString().equals("Required")){
+                cna_time_to.setText(R.string.validate_required)
+                textView15.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(cna_title.text.isEmpty()){
+                cna_title.setHint(R.string.validate_required)
+                textView14.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(cna_address.text.isEmpty()){
+                cna_address.setHint(R.string.validate_required)
+                textView16.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(cna_desc.text.isEmpty()){
+                cna_desc.setHint(R.string.validate_required)
+                textView17.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(!(cb_dryfood.isChecked||cb_fandv.isChecked||cb_fresh.isChecked||cb_frozen.isChecked||cb_meat.isChecked||cb_refriger.isChecked)){
+                tv_typefooderror.setText("(Pick At Least One)")
+                tv_typefooderror.setTextColor(Color.RED)
+                tv_typefooderror.isVisible = true
+                textView18.setTextColor(Color.RED)
+                validate = false
+            }
+
+            if(cna_butt_uploadphoto.text.toString().equals("Upload A Photo")){
+                cna_butt_uploadphoto.setTextColor(Color.RED)
+                validate = false
+            }
+
+
+
+            if(validate == true){
+                //uploadImageToFirebase()
+                createFKActivity()
+                Toast.makeText(this.requireContext(),"Activity is created",Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
     }
+
+    private fun createFKActivity(){
+
+
+
+    }
+
+    private fun uploadImageToFirebase (){
+        if(selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/image/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(context!!.contentResolver,selectedPhotoUri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            cna_butt_uploadphoto.setText("")
+            cna_butt_uploadphoto.setBackgroundDrawable(bitmapDrawable)
+
+        }
+
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
